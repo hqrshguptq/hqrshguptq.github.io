73cc727cf689762f6e8ae33334298f19ed03fec5
hqrshguptq <54597242+hqrshguptq@users.noreply.github.com>
2019-12-20 19:44:49 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2017, Codrops
- * http://www.codrops.com
- */
+
 ;(function(window) {
 
 	// Helper vars and functions.
